c606a032088335a7dcf26065ec58254066152035
Ladanenko1 <ladanenko99@mail.ru>
2023-12-21 22:06:54 +0300

    мЯуу(подправленох2)

--- PZ-7/PZ_7.6.1.py
+++ PZ-7/PZ_7.6.1.py
@@ -1,3 +1,4 @@
+# Дана строка. Преобразовать в ней все прописные латинские буквы в строчные.
 input_string = input("Введите строку: ")
 lowercase_string = input_string.lower()
 print(lowercase_string)
